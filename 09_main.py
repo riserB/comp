95bb6c45715f5f11f02624a35b1f90133a80faba
riserB <106135433+riserB@users.noreply.github.com>
2022-05-24 10:37:44 +0530

    Create 09_main.py

---
+++ 09_main.py
@@ -0,0 +1,9 @@
+# Python program to demonstrate
+# Read record in a file
+# Open function to open the file "myfile.txt"
+# it's reference in the variable file1
+file1 = open("myfile.txt")
+# Reading from file
+print(file1.read())
+# Closing file
+file1.close()
